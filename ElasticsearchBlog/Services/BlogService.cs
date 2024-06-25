17a3a661054ef9a0d5df044a26be72c902a8d240
M <M@DESKTOP>
2024-06-25 14:42:37 +0300

    Blog Service

---
+++ ElasticsearchBlog/Services/BlogService.cs
@@ -0,0 +1,33 @@
+﻿using ElasticsearchBlog.Models;
+using ElasticsearchBlog.Repositories;
+using ElasticsearchBlog.ViewModels;
+
+namespace ElasticsearchBlog.Services;
+
+public class BlogService
+{
+    private readonly BlogRepository blogRepository;
+
+    public BlogService(BlogRepository blogRepository)
+    {
+        this.blogRepository = blogRepository;
+    }
+
+    public async Task<bool> SaveAsync(BlogCreateViewModel model)
+    {
+        
+        
+        Blog newBlog = new Blog();
+        newBlog.Title = model.Title;
+        newBlog.UserId = Guid.NewGuid();
+        newBlog.Content = model.Content;
+        newBlog.Tags = model.Tags.ToArray();
+
+        var isCreated  = await blogRepository.SaveAsync(newBlog);
+
+        return isCreated != null;
+
+
+
+    }
+}
